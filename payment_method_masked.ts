6105a2c74d04c5b2ec5614f1bdac784245ef6b9f
Yuxuan Xie <teststaybaka@gmail.com>
2023-11-22 23:42:05 +0000

    Add an enum for CardBrand. Release

--- payment_method_masked.ts
+++ payment_method_masked.ts
@@ -1,7 +1,56 @@
-import { MessageDescriptor, PrimitiveType } from '@selfage/message/descriptor';
+import { EnumDescriptor, MessageDescriptor, PrimitiveType } from '@selfage/message/descriptor';
+
+export enum CardBrand {
+  AMEX = 1,
+  DINERS = 2,
+  DISCOVER = 3,
+  EFTPOS_AU = 4,
+  JCB = 5,
+  MASTERCARD = 6,
+  UNIONPAY = 7,
+  VISA = 8,
+}
+
+export let CARD_BRAND: EnumDescriptor<CardBrand> = {
+  name: 'CardBrand',
+  values: [
+    {
+      name: 'AMEX',
+      value: 1,
+    },
+    {
+      name: 'DINERS',
+      value: 2,
+    },
+    {
+      name: 'DISCOVER',
+      value: 3,
+    },
+    {
+      name: 'EFTPOS_AU',
+      value: 4,
+    },
+    {
+      name: 'JCB',
+      value: 5,
+    },
+    {
+      name: 'MASTERCARD',
+      value: 6,
+    },
+    {
+      name: 'UNIONPAY',
+      value: 7,
+    },
+    {
+      name: 'VISA',
+      value: 8,
+    },
+  ]
+}
 
 export interface CardMasked {
-  brand?: string,
+  brand?: CardBrand,
   lastFourDigits?: string,
   expMonth?: number,
   expYear?: number,
@@ -12,7 +61,7 @@ export let CARD_MASKED: MessageDescriptor<CardMasked> = {
   fields: [
     {
       name: 'brand',
-      primitiveType: PrimitiveType.STRING,
+      enumType: CARD_BRAND,
     },
     {
       name: 'lastFourDigits',
