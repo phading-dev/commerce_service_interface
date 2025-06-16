fed683248be5dae6d9fd90c561f69876dcbd5f4f
Ykuyo <ykuyo.dev@gmail.com>
2025-06-15 18:42:06 -0700

    Generated services. Release

--- service.ts
+++ service.ts
@@ -2,10 +2,10 @@ import { ServiceDescriptor } from '@selfage/service_descriptor';
 
 export let COMMERCE_WEB_SERVICE: ServiceDescriptor = {
   name: "CommerceWebService",
-  path: "/c/w",
+  path: "/cw",
 }
 
 export let COMMERCE_NODE_SERVICE: ServiceDescriptor = {
   name: "CommerceNodeService",
-  path: "/c/n",
+  path: "/cn",
 }
